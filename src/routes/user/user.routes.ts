54656ff94466a5d2e3c5051c61416fe9e4d71de8
hhchavada <harshchavada@174gmail.com>
2024-06-09 22:22:34 -0700

    type-script

---
+++ src/routes/user/user.routes.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { userVerifyToken } from "../../helpers/userVerifyToken";
+
+import {
+  registerUser,
+  loginUser,
+  getAllUser,
+  getUser,
+  updateUser,
+  deleteUser,
+  updatePassword
+} from "../../controller/user/user.controller";
+
+const userRoutes = express.Router();
+
+// REGISTER User
+userRoutes.post("/register-User", registerUser);
+
+// LOGIN User
+userRoutes.post("/login-User", loginUser);
+
+// GET ALL User
+userRoutes.get("/get-All-User", userVerifyToken , getAllUser);
+
+// GET User
+userRoutes.get("/get-User", userVerifyToken , getUser);
+
+// UPDATE User
+userRoutes.put("/update-User", userVerifyToken , updateUser);
+
+// DELETE User
+userRoutes.delete("/delete-User", userVerifyToken , deleteUser);
+
+// UPDATE PASSWORD
+userRoutes.put("/update-Password", userVerifyToken , updatePassword);
+
+export default userRoutes;
